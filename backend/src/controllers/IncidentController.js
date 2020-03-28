9cf838a58fbf3686216f030d32a8d80cfce53ec7
Edilson Novaes <edilson.novaesjr@gmail.com>
2020-03-28 01:20:33 -0300

    Front-end da aplicação

--- backend/src/controllers/IncidentController.js
+++ backend/src/controllers/IncidentController.js
@@ -45,9 +45,13 @@ module.exports = {
             .where('id', id)
             .select('ong_id')
             .first();
+        
+        if(!incident){
+            return response.status(400).json({ error: 'Incident inexisting.'});
+        }
 
         if (incident.ong_id != ong_id){
-            return response.status(401).json({ error: 'Operation not permitted'});
+            return response.status(401).json({ error: 'Operation not permitted.'});
         }
 
         await connection('incidents').where('id', id).delete();
